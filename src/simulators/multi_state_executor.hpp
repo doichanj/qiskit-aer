008e3efda32906e41e66d7c4f0b3c07ddfffb152
Jun Doi <doichan@jp.ibm.com>
2024-05-17 10:10:47 +0900

    test

--- src/simulators/multi_state_executor.hpp
+++ src/simulators/multi_state_executor.hpp
@@ -536,6 +536,7 @@ void MultiStateExecutor<state_t>::run_circuit_with_shot_branching(
               continue;
 
             branches[istate]->advance_iterator();
+            branches[istate]->creg() = state.creg();
             if (op->sample_noise) {
               // runtime noise sampling
               branches[istate]->apply_runtime_noise_sampling(state.creg(), *op,
